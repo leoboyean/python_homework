788b76b82ce99df05ca0f637aecf98d6868227c0
APPE <leoboyean.ap@gmail.com>
2023-03-13 19:03:46 -0600

    [update & new] - update readme and new file to python

---
+++ 00_Introduction/hello_world.py
@@ -0,0 +1,29 @@
+#Ejercicio 1 make an exce types and solutions
+
+
+#hacer una modificacion de cada uno de esos elementos 
+
+
+
+#suma
+#resta
+#multiplicacion
+#Division
+#Exponencial
+#modular
+print(84//3) #division entera   28
+#Bool
+print( )
+#Tipos de datos 7
+
+
+#Type(1)
+
+print(type(3.5 * 9.78))#Int
+#Float
+print(type(1j + 3 + 5j))#Complex // Complejo only works with j
+#String
+#List
+#Dictionary
+print(type({9.8, 3.14, 2.7}))#Set // Conjunto 
+print(type((9.8, 3.14, 2.7)))#tuble // Tupla
--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # python_homework
 
 From Python Workshop LITUX-UAZ 2023
-
+[Unciafidelis - Python_Workshop](https://github.com/unciafidelis/Python_workshop_2023)
 
 
 
